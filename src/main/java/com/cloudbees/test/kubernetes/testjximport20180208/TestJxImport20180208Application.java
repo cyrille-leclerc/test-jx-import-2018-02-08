9a20f33fe3230cf7d8b41697e5842b0510c90f98
Cyrille Le Clerc <cleclerc@cloudbees.com>
2018-02-08 22:55:18 +0100

    Initial import

---
+++ src/main/java/com/cloudbees/test/kubernetes/testjximport20180208/TestJxImport20180208Application.java
@@ -0,0 +1,12 @@
+package com.cloudbees.test.kubernetes.testjximport20180208;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJxImport20180208Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJxImport20180208Application.class, args);
+	}
+}
---
+++ src/test/java/com/cloudbees/test/kubernetes/testjximport20180208/TestJxImport20180208ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cloudbees.test.kubernetes.testjximport20180208;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestJxImport20180208ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
